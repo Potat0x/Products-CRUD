18f124e34f63b4b86bf3e91772669e9147c848ad
Potat0x <Potat0x@users.noreply.github.com>
2019-05-31 16:09:21 +0200

    Fix Swagger UI 404

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,10 +15,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("SampleText API")
+                .description("Spring Boot products CRUD")
+                .build();
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/products/**"))
-                .build().apiInfo(new ApiInfo("SampleText API", "Spring Boot CRUD", "1.0", "", "", "", ""));
+                .build()
+                .apiInfo(apiInfo);
     }
 }
